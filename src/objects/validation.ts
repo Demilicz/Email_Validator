612212cee1145aab279b7d8360f1c0acb563d49b
Demilicz <Necrolick92@gmail.com>
2021-09-08 20:15:49 +0200

    something wrong with this typescript

--- src/objects/validation.ts
+++ src/objects/validation.ts
@@ -1,15 +1,11 @@
  const patterns: object =
 {
-
-
   fullname: /^\w{2,20}\s\s?\w{2,20}$/,
   nickname: /^\w{2,20}/,
-  email: /$[a-d]^/,
-  password: /$[a-d]^/,
+  email: /^$/,
+  password: /^$/,
   telephone: /^[1-9]\d{10}$/
-
 };
-console.log( patterns);
 
 
 export default patterns;
--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -2,24 +2,24 @@
   <div class="registration">
     <h2>New User Signup</h2>
     <form action="#">
-      <input type="name" placeholder="Enter your fullname" >
+      <input type="name" name="name" @keyup="validName" placeholder="Enter your fullname" >
       <p>Firstname and lastname must be alphanumeric and contains 5-12 characters</p>
 
-      <input type="nickname" placeholder="Enter your nickname" >
+      <input type="nickname" name="kickname" placeholder="Enter your nickname" >
       <p>Nickname must be alphanumeric and contains 5-12 characters</p>
 
-      <input type="email" placeholder="Enter your email" >
+      <input type="email" name="email" placeholder="Enter your email" >
       <p>Email must be valid address e.g.  me@mydomain.com</p>
 
-      <input type="password" placeholder="Enter your password" >
-      <input type="password" placeholder="Repeat your password" >
+      <input type="password" name="password" placeholder="Enter your password" >
+      <input type="password" name="cheakPassword" placeholder="Repeat your password" >
       <p>Password must be alphanumeric (@ , _ and are also allowed) and be 8-20 characters</p>
 
-      <input type="telephon" placeholder="Enter your telephon">
+      <input type="telephon" name="telephon" placeholder="Enter your telephon">
       <p>Telephon must be a valid telphone number 11 digits</p>
 
     </form>
-    <button @click="click">Confirm</button>
+    <button>Confirm</button>
   </div>
 </template>
 
@@ -30,11 +30,20 @@
   import validation from '../objects/validation';
 
   export default {
+    data: () =>({
+      name: null,
+      nickname: null,
+      email: null,
+      password: null,
+      cheakPassword: null,
+      telephon: null
+    }),
     methods: {
-      click() {
-        console.log(validation);
+      validName() {
+       console.log(validation);
 
       }
+
     }
   }
 </script>
